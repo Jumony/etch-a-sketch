25b5a76b67cdd60f9f4ba14ae8b60b7aeaed51dd
Jumony <fireball2489@gmail.com>
2024-05-03 15:36:58 -0400

    User can now toggle between rainbow colors and black. Also added a few comments

--- eas.js
+++ eas.js
@@ -2,6 +2,7 @@ let display_size = 480;
 let gridSize = 16; // Number of squares per row or column
 let squareSize = display_size / gridSize; // Calculate the size of each square
 let currentSize = document.createElement('div');
+let default_color = true;
 
 // construct the etch-a-sketch
 const etch_a_sketch = document.querySelector('#container');
@@ -19,21 +20,23 @@ resizebtn.classList.add('resizebtn');
 resizebtn.style.marginBottom = '30px';
 etch_a_sketch.insertAdjacentElement('beforebegin', resizebtn)
 
+// Color Button
+const choose_color_btn = document.createElement('button');
+choose_color_btn.textContent = 'Toggle Rainbow Mode';
+choose_color_btn.classList.add('choose_color_btn');
+choose_color_btn.style.marginBottom = '30px';
+etch_a_sketch.insertAdjacentElement('beforebegin', choose_color_btn);
+
 // Sets defaults
 CreateGridBlocks(gridSize, squareSize);
 const gridBlocks = document.querySelectorAll('.grid');
 
-gridBlocks.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        element.style.backgroundColor = 'blue';
-    })
-});
 
 document.body.appendChild(currentSize);
 currentSize.textContent = `Current Size: ${gridSize}x${gridSize}`
 //------------------------------------------
 
-
+// Resize btn functionality
 resizebtn.addEventListener('click', () => {
     gridSize = prompt('Please enter the desired size of the display (1-100)');
     while (gridSize <= 0 || gridSize > 100)
@@ -44,6 +47,11 @@ resizebtn.addEventListener('click', () => {
     CreateGridBlocks(gridSize, squareSize);
     currentSize.textContent = `Current Size: ${gridSize}x${gridSize}`;
 
+})
+
+// Color Button Functionality
+choose_color_btn.addEventListener('click', () => {
+    default_color = !default_color;
 })
 
 
@@ -65,9 +73,13 @@ function CreateGridBlocks (gridLength, squareSize)
     }
 
     etch_a_sketch.addEventListener('mouseover', (event) => {
-        if (event.target.classList.contains('grid')) { 
+        if (event.target.classList.contains('grid') && !default_color) { 
             let randomColor = Math.floor(Math.random()*16777215).toString(16);
             event.target.style.backgroundColor = '#' + randomColor;
+        }
+        else if (event.target.classList.contains('grid') && default_color) {
+            let color = 'black';
+            event.target.style.backgroundColor = color;
         }
     })
 }
